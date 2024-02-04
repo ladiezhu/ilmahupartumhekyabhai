a6c48036c140057ee8ac8bdf96b3460eca0d7867
ladiezhu <158833147+ladiezhu@users.noreply.github.com>
2024-02-04 21:26:09 +0530

    Delete file directory

--- file/script.js
+++
@@ -1,9 +0,0 @@
-
-function handleNoClick() {
-    document.getElementById('noButton').innerText = 'STFU! Stop lying, you are mad at me';
-}
-
-function redirectToApology() {
-    window.location.href = 'file:///C:/Users/ILMA/Desktop/New%20folder/index2.html';
-}
-
