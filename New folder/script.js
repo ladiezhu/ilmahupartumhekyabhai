515062f8a39e9c2bd3ef21222119b61025ef2e12
ladiezhu <158833147+ladiezhu@users.noreply.github.com>
2024-02-04 21:01:32 +0530

    Add files via upload

---
+++ New folder/script.js
@@ -0,0 +1,9 @@
+
+function handleNoClick() {
+    document.getElementById('noButton').innerText = 'STFU! Stop lying, you are mad at me';
+}
+
+function redirectToApology() {
+    window.location.href = 'file:///C:/Users/ILMA/Desktop/New%20folder/index2.html';
+}
+
